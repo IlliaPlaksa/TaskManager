3e0a53a2c024afecafff45b9dee3ce69f71b6ff4
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-01 14:56:23 +0200

    Changed Steps using Readers functions

--- src/cli/imp/steps/AddStep.cpp
+++ src/cli/imp/steps/AddStep.cpp
@@ -6,6 +6,39 @@
 
 Step::Status AddStep::Execute(Context &context)
 {
+    auto console = this->GetConsoleManipulator();
+    auto task_struct = context.GetStruct();
+
+    console.ResetPrompt("add Task");
+
+    auto title = Read::Title(console);
+    auto date = Read::Date(console);
+    auto priority = Read::Priority(console);
+    auto label = Read::Label(console);
+    console.ResetPrompt("add Parent");
+    auto id = Read::Id(console);
+
+    auto confirm = Read::Confirm(console);
+    if (confirm)
+    {
+        if (confirm.value())
+        {
+            // Filling structure
+            task_struct->SetTitle(title.value());
+            task_struct->SetDate(date.value());
+            task_struct->SetPriority(priority.value());
+            task_struct->SetLabel(label.value());
+            task_struct->SetStatus(Task::Status::kInProgress);
+
+            if (task_struct->IsReadyToConstruct())
+                task_struct->ConstructTask(); // TODO implement sending task to MODEL
+        } else
+            console.WriteLine("Operation canceled");
+
+    }
+
+    console.ResetPrompt();
+
     context.SetStep(context.GetFactory()->CreateStep(StepId::kRoot));
     return Status::kOk;
 }
--- src/cli/imp/steps/RootStep.cpp
+++ src/cli/imp/steps/RootStep.cpp
@@ -7,21 +7,12 @@
 Step::Status RootStep::Execute(Context &context)
 {
     auto console = this->GetConsoleManipulator();
-
-    std::shared_ptr<Step> new_state;
-
     auto step_id = Read::Command(console);
 
-    if (step_id)
-        new_state = context.GetFactory()->CreateStep(step_id.value());
-    else
-    {
-        console.WriteLine("No such command\nUse [help] to determine command you needed");
-        new_state = context.GetFactory()->CreateStep(StepId::kRoot);
-    }
+    auto next_state = context.GetFactory()->CreateStep(step_id.value());
 
-    context.SetStep(new_state);
-    if (new_state)
+    context.SetStep(next_state);
+    if (next_state)
         return Status::kOk;
     else
         return Status::kError;
