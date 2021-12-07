403d120daf459435061dd78dcfd2bd181ead9bd6
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-07 16:11:10 +0200

    Minor changes in Readers
    Changed argument of methods to shared pointer

--- src/cli/imp/Readers.cpp
+++ src/cli/imp/Readers.cpp
@@ -4,109 +4,109 @@
 
 #include "../include/Readers.h"
 
-StepId Read::Command(const ConsoleManipulator &console)
+StepId Read::Command(const std::shared_ptr<ConsoleManipulator> &console)
 {
-    auto input = Validate::Command(console.ReadLine());
+    auto input = Validate::Command(console->ReadLine());
 
     while (!input)
     {
-        console.WriteLine("No such command\nUse [help] to determine command you needed");
-        input = Validate::Command(console.ReadLine());
+        console->WriteLine("No such command\nUse [help] to determine command you needed");
+        input = Validate::Command(console->ReadLine());
     }
     return input.value();
 }
-std::string Read::Title(const ConsoleManipulator &console)
+std::string Read::Title(const std::shared_ptr<ConsoleManipulator> &console)
 {
     std::optional<std::string> input;
     input = Validate::Title(
-        console.ReadLine("[Title]")
+        console->ReadLine("[Title]")
     );
 
     while (!input)
     {
-        console.WriteLine("Wrong title passed");
+        console->WriteLine("Wrong title passed");
         input = Validate::Title(
-            console.ReadLine("[Title]")
+            console->ReadLine("[Title]")
         );
     }
     return input.value();
 }
-TaskId Read::Id(const ConsoleManipulator &console)
+TaskId Read::Id(const std::shared_ptr<ConsoleManipulator> &console)
 {
     const std::string message = "[ID]";
     std::optional<TaskId> input;
     input = Validate::Id(
-        console.ReadLine(message)
+        console->ReadLine(message)
     );
 
     while (!input)
     {
-        console.WriteLine("Wrong ID passed");
+        console->WriteLine("Wrong ID passed");
         input = Validate::Id(
-            console.ReadLine(message)
+            console->ReadLine(message)
         );
     }
     return input.value();
 }
-time_t Read::Date(const ConsoleManipulator &console)
+time_t Read::Date(const std::shared_ptr<ConsoleManipulator> &console)
 {
     const std::string message = "[Date] DD.MM.YY";
     auto input = Validate::Date(
-        console.ReadLine(message)
+        console->ReadLine(message)
     );
 
     while (!input)
     {
-        console.WriteLine("Wrong Date passed");
+        console->WriteLine("Wrong Date passed");
         input = Validate::Date(
-            console.ReadLine(message)
+            console->ReadLine(message)
         );
     }
     return input.value();
 }
-Task::Priority Read::Priority(const ConsoleManipulator &console)
+Task::Priority Read::Priority(const std::shared_ptr<ConsoleManipulator> &console)
 {
     std::string message = "[Priority]";
     auto input = Validate::Priority(
-        console.ReadLine(message)
+        console->ReadLine(message)
     );
 
     while (!input)
     {
-        console.WriteLine("Wrong Priority passed");
+        console->WriteLine("Wrong Priority passed");
         input = Validate::Priority(
-            console.ReadLine(message)
+            console->ReadLine(message)
         );
     }
     return input.value();
 }
-std::string Read::Label(const ConsoleManipulator &console)
+std::string Read::Label(const std::shared_ptr<ConsoleManipulator> &console)
 {
     std::string message = "[Label]";
     auto input = Validate::Label(
-        console.ReadLine(message)
+        console->ReadLine(message)
     );
     while (!input)
     {
-        console.WriteLine("Wrong Label passed");
+        console->WriteLine("Wrong Label passed");
         input = Validate::Label(
-            console.ReadLine(message)
+            console->ReadLine(message)
         );
     }
     return input.value();
 }
-bool Read::Confirm(const ConsoleManipulator &console)
+bool Read::Confirm(const std::shared_ptr<ConsoleManipulator> &console)
 {
     std::string message = "[Confirm] \"y/n\"";
     auto input = Validate::Confirm(
-        console.ReadLine(message)
+        console->ReadLine(message)
     );
 
     while (!input)
     {
-        console.WriteLine("Wrong Confirmation passed");
+        console->WriteLine("Wrong Confirmation passed");
         input = Validate::Confirm(
-            console.ReadLine(message)
+            console->ReadLine(message)
         );
     }
     return input.value();
--- src/cli/include/Readers.h
+++ src/cli/include/Readers.h
@@ -16,13 +16,13 @@ enum class StepId;
 
 namespace Read
 {
-    StepId Command(const ConsoleManipulator &console);
-    std::string Title(const ConsoleManipulator &console);
-    TaskId Id(const ConsoleManipulator &console);
-    Task::Priority Priority(const ConsoleManipulator &console);
-    time_t Date(const ConsoleManipulator &console);
-    std::string Label(const ConsoleManipulator &console);
-    bool Confirm(const ConsoleManipulator &console);
+    StepId Command(const std::shared_ptr<ConsoleManipulator> &console);
+    std::string Title(const std::shared_ptr<ConsoleManipulator> &console);
+    TaskId Id(const std::shared_ptr<ConsoleManipulator> &console);
+    Task::Priority Priority(const std::shared_ptr<ConsoleManipulator> &console);
+    time_t Date(const std::shared_ptr<ConsoleManipulator> &console);
+    std::string Label(const std::shared_ptr<ConsoleManipulator> &console);
+    bool Confirm(const std::shared_ptr<ConsoleManipulator> &console);
 }
 
 #endif //TASKMANAGER_SRC_CLI_INCLUDE_READERS_H_
