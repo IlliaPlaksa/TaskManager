af00388b9482e984dc948d03ffef4feb364123c1
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-13 12:37:38 +0200

    Added implementation for ShowCommand

--- src/controller/imp/commands/ShowCommand.cpp
+++ src/controller/imp/commands/ShowCommand.cpp
@@ -8,8 +8,28 @@ Response ShowCommand::Execute(const std::shared_ptr<IView> &view)
 {
     auto result = Response{};
 
-    // Command code here
+    auto task_storage = TaskStorage{};
 
-    result.status = Response::Status::kSuccess;
+    try
+    {
+        auto model = GetModel();
+        auto parents = model->ShowParents();
+
+        for (const auto& elem : parents)
+            task_storage.Add(TaskId::CreateDefault(), parents);
+
+        for (const auto& elem : parents)
+            task_storage.Add(elem.first,
+                             model->ShowChild(elem.first));
+
+        view->LoadTasks(task_storage);
+        result.status = Response::Status::kSuccess;
+    }
+    catch (const std::exception &e)
+    {
+        result.status = Response::Status::kError;
+        result.error_message = e.what();
+    }
+
     return result;
 }
