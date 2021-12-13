574eb22373091e623e16d5e811b4009d8c170e1a
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-13 11:53:15 +0200

    Added implementation for AddCommand

--- src/controller/imp/commands/AddCommand.cpp
+++ src/controller/imp/commands/AddCommand.cpp
@@ -8,11 +8,23 @@ Response AddCommand::Execute(const std::shared_ptr<IView> &view)
 {
     auto result = Response{};
 
-    // Command code here
     auto task_struct = view->GetTaskStruct();
 
-    GetModel()->Add(task_struct->ConstructTask());
+    auto task = task_struct->ConstructTask();
+    auto parent_id = task_struct->GetParent();
 
-    result.status = Response::Status::kSuccess;
+    try
+    {
+        if (GetModel()->AddSubTask(task, parent_id).value())
+            result.status = Response::Status::kSuccess;
+        else
+            result.status = Response::Status::kError;
+    }
+    catch (const std::exception &e)
+    {
+        result.status = Response::Status::kError;
+        result.error_message = e.what();
+    }
+
     return result;
 }
