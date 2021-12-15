014f82d4c1bd6529fda56b5946b1c647d2ca4d62
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-15 16:31:45 +0200

    Added blank Commands tests

---
+++ tests/controller/commands/DeleteCommandTest.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Illia Plaksa on 14.12.2021.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../../../src/controller/include/ConcreteCommands.h"
+#include "../mocks/ModelMock.h"
+#include "../mocks/ViewMock.h"
+
+class DeleteCommandTest : ::testing::Test {};
+
+TEST(DeleteCommandTest, shouldCreate)
+{
+//    auto model = std::shared_ptr<IModel>{new ModelMock};
+//    auto view = std::shared_ptr<IView>{new ViewMock};
+//
+//    auto command = CompleteCommand{model};
+//
+//    EXPECT_CALL(*model, ())
+//    .Times(1)
+//    .WillOnce(testing::Return());
+//
+//    command.Execute(view);
+}
