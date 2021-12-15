335bb70dc56c63c947c827a43a9d69df30d40836
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-15 16:16:08 +0200

    Created AddCommand tests

---
+++ tests/controller/commands/AddCommandTest.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Illia Plaksa on 14.12.2021.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../../../src/controller/include/ConcreteCommands.h"
+#include "../mocks/ModelMock.h"
+#include "../mocks/ViewMock.h"
+
+class AddCommandTest : ::testing::Test {};
+
+TEST(AddCommandTest, shouldCreate)
+{
+    auto model = std::shared_ptr<ModelMock>{new ModelMock};
+    auto view = std::shared_ptr<ViewMock>{new ViewMock};
+
+    auto command = AddCommand{model};
+
+    auto task = Task::Create("Title",
+                             time(nullptr),
+                             Task::Priority::kHigh);
+    auto id = TaskId::CreateDefault();
+
+    auto task_struct = TaskStruct{};
+    task_struct
+        .SetFromTask(task)
+        .SetParent(id);
+
+    EXPECT_CALL(*view, GetTaskStruct())
+        .Times(1)
+        .WillOnce(testing::Return(task_struct));
+
+    EXPECT_CALL(*model, Add(task, id))
+        .Times(1)
+        .WillOnce(testing::Return(TaskId::CreateDefault()));
+
+    command.Execute(view);
+}
