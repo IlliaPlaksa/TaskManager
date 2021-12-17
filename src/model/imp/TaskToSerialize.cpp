6530aae6d40801c0ad5a69a41868371a304d82d0
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-17 14:41:08 +0200

    Added TaskToSerialize source files to model

---
+++ src/model/imp/TaskToSerialize.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Illia Plaksa on 17.12.2021.
+//
+#include "../include/TaskToSerialize.h"
+
+std::optional<TaskToSerialize> CreateTaskToSerialize(const TaskId &id,
+                                                     const Task &task)
+{
+    auto result = TaskToSerialize{};
+    if (id.IsInitialized() and task.IsInitialized())
+    {
+        // Allocation of struct fields
+        auto tmp_id = std::make_unique<TaskId>(id);
+        auto tmp_task = std::make_unique<Task>(task);
+
+        result.set_allocated_id(tmp_id.release());
+        result.set_allocated_task(tmp_task.release());
+
+    } else
+        return std::nullopt;
+
+    return result;
+}
+
+std::optional<TaskToSerialize> CreateSubTaskToSerialize(const TaskId &id,
+                                                        const Task &task,
+                                                        const TaskId &parent_id)
+{
+    auto result = CreateTaskToSerialize(id, task);
+    if (result)
+    {
+        auto tmp_parent_id = std::make_unique<TaskId>(parent_id);
+        result->set_allocated_parent_id(tmp_parent_id.release());
+    } else
+        return std::nullopt;
+
+    return result;
+}
+
+bool operator==(const TaskToSerialize &first, const TaskToSerialize &second)
+{
+    return first.SerializeAsString() == second.SerializeAsString();
+}
+
---
+++ src/model/include/TaskToSerialize.h
@@ -0,0 +1,19 @@
+//
+// Created by Illia Plaksa on 17.12.2021.
+//
+
+#ifndef TASKMANAGER_SRC_MODEL_INCLUDE_TASKTOSERIALIZE_H_
+#define TASKMANAGER_SRC_MODEL_INCLUDE_TASKTOSERIALIZE_H_
+
+#include "TaskToSerialize.pb.h"
+
+std::optional<TaskToSerialize> CreateTaskToSerialize(const TaskId &id,
+                                                     const Task &task);
+
+std::optional<TaskToSerialize> CreateSubTaskToSerialize(const TaskId &id,
+                                                        const Task &task,
+                                                        const TaskId &parent_id);
+
+bool operator==(const TaskToSerialize &first, const TaskToSerialize &second);
+
+#endif //TASKMANAGER_SRC_MODEL_INCLUDE_TASKTOSERIALIZE_H_
