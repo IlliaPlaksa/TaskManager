36f26c667ef40e61b62cd9e1fbacb8377129923b
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-17 14:42:40 +0200

    Replaced Task class to protobuf Task
    Changes:
    - deleted Task class
    - added static functions for protobuf Task

--- src/model/imp/Task.cpp
+++ src/model/imp/Task.cpp
@@ -4,96 +4,29 @@
 
 #include "../include/Task.h"
 
-Task::Task(
-    const std::string &title,
-    const std::time_t &due_to_date,
-    const Priority &priority,
-    const std::string &label,
-    const Status &status)
-    :
-    title_(title),
-    due_to_date_(due_to_date),
-    priority_(priority),
-    label_(label),
-    status_(status) {}
-
-Task Task::Create(const std::string &title,
-                  const std::time_t &due_to_date,
-                  const Priority &priority,
-                  const std::string &label,
-                  const Status &status)
+std::optional<Task> CreateTask(const std::string &title,
+                               const std::time_t &due_to_date,
+                               const Task_Priority &priority,
+                               const std::string &label,
+                               const Task_Status &status)
 {
-    if (!title.empty())
-        return Task{title, due_to_date, priority, label, status};
-    else
-        throw std::runtime_error("Passed empty title");
-}
-
-std::string Task::GetTitle() const
-{
-    return this->title_;
-}
-std::time_t Task::GetDate() const
-{
-    return this->due_to_date_;
-}
-Task::Priority Task::GetPriority() const
-{
-    return this->priority_;
-}
-std::string Task::GetLabel() const
-{
-    return this->label_;
-}
-Task::Status Task::GetStatus() const
-{
-    return this->status_;
-}
-Task Task::Complete(const Task &task)
-{
-    return Task::Create(task.GetTitle(),
-                        task.GetDate(),
-                        task.GetPriority(),
-                        task.GetLabel(),
-                        Task::Status::kCompleted);
-}
-std::string Task::ToString() const
-{
-    std::time_t date = this->GetDate();
-
-    std::stringstream output;
-    output
-        << "Title: " << this->GetTitle() << '\t'
-        << "Due date: " << std::put_time(std::localtime(&date), "%d.%m.%Y") << '\t'
-        << "Priority: " << PriorityToString(this->GetPriority()) << '\t'
-        << "Label: " << this->GetLabel() << '\t'
-        << "Status: " << StatusToString(this->GetStatus()) << '\t';
-    return output.str();
-}
-std::string Task::PriorityToString(const Task::Priority &priority)
-{
-    switch (priority)
+    auto result = Task{};
+    if (!title.empty() and due_to_date >= 0)
     {
-        case Task::Priority::kHigh: return "High";
-        case Task::Priority::kMedium: return "Medium";
-        case Task::Priority::kLow: return "Low";
-        default: return std::string{};
-    }
+        result.set_title(title);
+        result.set_status(status);
+        result.set_label(label);
+
+        auto tmp_date = std::make_unique<google::protobuf::Timestamp>();
+        tmp_date->set_seconds(due_to_date);
+        result.set_allocated_due_date(tmp_date.release());
+    } else
+        return std::nullopt;
+
+    return result;
 }
-std::string Task::StatusToString(const Task::Status &status)
+
+bool operator==(const Task &first, const Task &second)
 {
-    switch (status)
-    {
-        case Task::Status::kInProgress: return "In progress";
-        case Task::Status::kCompleted: return "Completed";
-        default: return std::string{};
-    }
-}
-bool Task::operator==(const Task &other) const
-{
-    return this->GetTitle() == other.GetTitle() &&
-        this->GetPriority() == other.GetPriority() &&
-        this->GetLabel() == other.GetLabel() &&
-        this->GetStatus() == other.GetStatus() &&
-        this->GetDate() == other.GetDate();
+    return first.SerializeAsString() == second.SerializeAsString();
 }
--- src/model/include/Task.h
+++ src/model/include/Task.h
@@ -5,65 +5,14 @@
 #ifndef TASKMANAGER_SRC_MODEL_TASK_H_
 #define TASKMANAGER_SRC_MODEL_TASK_H_
 
-#include <string>
-#include <sstream>
-#include <ctime>
-#include <iomanip>
+#include "Task.pb.h"
 
-class Task
-{
-public: // internals
-    enum class Priority
-    {
-        kNone,
-        kHigh,
-        kMedium,
-        kLow
-    };
+std::optional<Task> CreateTask(const std::string &title,
+                               const std::time_t &due_to_date,
+                               const Task_Priority &priority,
+                               const std::string &label = "",
+                               const Task_Status &status = Task_Status_kInProgress);
 
-    enum class Status
-    {
-        kNone,
-        kCompleted,
-        kInProgress,
-    };
-
-public:
-    static Task Create(const std::string &title,
-                       const std::time_t &due_to_date,
-                       const Priority &priority,
-                       const std::string &label = "",
-                       const Status &status = Status::kInProgress);
-
-    static Task Complete(const Task &task);
-public: // Getters
-    std::string GetTitle() const;
-    std::time_t GetDate() const;
-    Task::Priority GetPriority() const;
-    std::string GetLabel() const;
-    Status GetStatus() const;
-
-public: // ToString methods
-    std::string ToString() const;
-    static std::string PriorityToString(const Task::Priority &priority);
-    static std::string StatusToString(const Task::Status &status);
-
-public:
-    bool operator==(const Task &other) const;
-
-private:
-    Task(const std::string &title,
-         const std::time_t &due_to_date,
-         const Priority &priority,
-         const std::string &label,
-         const Status &status);
-
-private:
-    std::string title_;
-    Priority priority_;
-    std::time_t due_to_date_;
-    std::string label_;
-    Status status_;
-};
+bool operator==(const Task &first, const Task &second);
 
 #endif //TASKMANAGER_SRC_MODEL_TASK_H_
