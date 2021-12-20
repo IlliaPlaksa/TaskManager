cbce481b55e3430bcd8f2ef7375b0396f9a7b810
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-20 13:16:26 +0200

    Changed ShowCommand

--- src/controller/imp/commands/ShowCommand.cpp
+++ src/controller/imp/commands/ShowCommand.cpp
@@ -6,30 +6,20 @@
 
 Response ShowCommand::Execute(const std::shared_ptr<IView> &view)
 {
-    auto result = Response{};
-
+    Response result;
     auto task_storage = TaskStorage{};
+    auto model = GetModel();
+    auto root = model->ShowParents();
 
-    try
+    result = task_storage.LoadRootTasks(model->ShowParents());
+
+    for (const auto &task: root)
     {
-        auto model = GetModel();
-        auto parents = model->ShowParents();
+        const auto &parent_id = task.parent_id();
+        task_storage.LoadSubTasks(parent_id, model->ShowChild(parent_id));
+    }
 
-        for (const auto& elem : parents)
-            task_storage.Add(TaskId::CreateDefault(), parents);
-
-        for (const auto& elem : parents)
-            task_storage.Add(elem.first,
-                             model->ShowChild(elem.first));
-
+    if (!result.IsError())
         view->LoadTasks(task_storage);
-        result.status = Response::Status::kSuccess;
-    }
-    catch (const std::exception &e)
-    {
-        result.status = Response::Status::kError;
-        result.error_message = e.what();
-    }
-
     return result;
 }
