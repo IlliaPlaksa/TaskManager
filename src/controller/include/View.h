1489e984877ba4b664955d813a9bf60e95aa3ca7
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-22 12:11:27 +0200

    Renamed IView interface to View

--- src/controller/include/View.h
+++ src/controller/include/View.h
@@ -2,14 +2,14 @@
 // Created by Illia Plaksa on 03.12.2021.
 //
 
-#ifndef TASKMANAGER_SRC_CONTROLLER_INCLUDE_IVIEW_H_
-#define TASKMANAGER_SRC_CONTROLLER_INCLUDE_IVIEW_H_
+#ifndef TASKMANAGER_SRC_CONTROLLER_INCLUDE_VIEW_H_
+#define TASKMANAGER_SRC_CONTROLLER_INCLUDE_VIEW_H_
 
 #include <optional>
 #include "../../model/include/TaskToSerialize.h"
 #include "cli/include/TaskStorage.h"
 
-class IView :public std::enable_shared_from_this<IView>
+class View :public std::enable_shared_from_this<View>
 {
 public:
     virtual void Run() = 0;
@@ -18,7 +18,7 @@ public:
 public:
     virtual std::optional<TaskToSerialize> GetTaskStruct() const = 0;
 public:
-    virtual std::shared_ptr<IView> shared() = 0;
+    virtual std::shared_ptr<View> shared() = 0;
 };
 
-#endif //TASKMANAGER_SRC_CONTROLLER_INCLUDE_IVIEW_H_
+#endif //TASKMANAGER_SRC_CONTROLLER_INCLUDE_VIEW_H_
