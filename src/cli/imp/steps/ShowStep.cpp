73423a94d41dd7d172bf0e45ea55640885dfbd1a
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-22 12:10:13 +0200

    Added private output helper methods to show command

--- src/cli/imp/steps/ShowStep.cpp
+++ src/cli/imp/steps/ShowStep.cpp
@@ -16,13 +16,11 @@ StepResult ShowStep::Execute(Context &context)
     {
         for (const auto &task: task_storage->GetRootTasks())
         {
+            //
             std::stringstream output;
-            output << std::to_string(task.id().value()) << ". " << task.DebugString();
+            output << ToString(task);
 
-            for (const auto &elem: task_storage->GetSubTasks(task.id()))
-            {
-                output << '\t' << std::to_string(elem.id().value()) << ". " << elem.DebugString();
-            }
+            OutputSubTasks(output, task.id(), *task_storage);
 
             console->WriteLine(output.str());
         }
@@ -32,3 +30,46 @@ StepResult ShowStep::Execute(Context &context)
     result.command_type = CommandType::kNone;
     return result;
 }
+std::string ShowStep::ToString(const Task::Priority &priority)
+{
+    switch (priority)
+    {
+        case Task_Priority_kHigh: return "High";
+        case Task_Priority_kMedium: return "Medium";
+        case Task_Priority_kLow: return "Low";
+        default: return std::string{};
+    }
+}
+std::string ShowStep::ToString(const Task::Status &status)
+{
+    switch (status)
+    {
+        case Task_Status_kCompleted: return "Completed";
+        case Task_Status_kInProgress: return "In progress";
+        default: return std::string{};
+    }
+}
+std::string ShowStep::ToString(const TaskToSerialize &task)
+{
+    std::stringstream output;
+
+    output << "ID: " << task.id().value() << " "
+           << "Title: " << task.task().title() << " "
+           << "Due date: " << google::protobuf::util::TimeUtil::ToString(task.task().due_date()) << " "
+           << "Priority: " << ToString(task.task().priority()) << " "
+           << "Status: " << ToString(task.task().status()) << " "
+           << "Label: " << task.task().label();
+
+    return output.str();
+}
+void ShowStep::OutputSubTasks(std::ostream &output, const TaskId &parent_id, const TaskStorage &storage)
+{
+    output << "\n\t";
+    for (const auto& task : storage.GetSubTasks(parent_id))
+    {
+        output << ToString(task);
+        OutputSubTasks(output, task.id(), storage);
+    }
+}
+
+
--- src/cli/include/MachineSteps.h
+++ src/cli/include/MachineSteps.h
@@ -65,9 +65,33 @@ class ShowStep : public Step
 public:
     using Step::Step;
     StepResult Execute(Context &context) override;
+
+private:
+    static void OutputSubTasks(std::ostream &output,
+                        const TaskId &task_id,
+                        const TaskStorage &storage
+    );
+
+    static std::string ToString(const TaskToSerialize &task);
+    static std::string ToString(const Task::Priority &priority);
+    static std::string ToString(const Task::Status &status);
 };
 
 class UpdateStep : public Step
+{
+public:
+    using Step::Step;
+    StepResult Execute(Context &context) override;
+};
+
+class SaveStep : public Step
+{
+public:
+    using Step::Step;
+    StepResult Execute(Context &context) override;
+};
+
+class LoadStep : public Step
 {
 public:
     using Step::Step;
