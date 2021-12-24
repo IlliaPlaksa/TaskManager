0e96e82300236749a1efd945346fab69a92f0f63
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-24 14:41:34 +0200

    Added VariableSet struct for Context

---
+++ src/cli/impl/VariableSet.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Illia Plaksa on 23.12.2021.
+//
+
+#include "../include/VariableSet.h"
+#include "../../util/TaskDTOCreators.h"
+#include "../../util/TaskCreators.h"
+
+
+std::optional<TaskDTO> VariableSet::MakeTaskDTO() const
+{
+    auto task = CreateTask(title, date, priority, label, status);
+    if (task)
+        return parent_id.has_value() ? CreateSubTaskDTO(id, *task, *parent_id)
+                                     : CreateTaskDTO(id, *task);
+    else
+        return std::nullopt;
+}
