2c2a43c135aade007d80c41c6ab718907386ed17
IlliaPLaksa <illia.plaksa@teamdev.com>
2021-12-24 15:05:59 +0200

    Changed ConcreteCommands
    Added CommandWithContext base class for Command subclasses.
    Added corresponding changes in concrete commands' interface

--- src/controller/include/ConcreteCommands.h
+++ src/controller/include/ConcreteCommands.h
@@ -2,47 +2,61 @@
 // Created by Illia Plaksa on 10.12.2021.
 //
 
-#ifndef TASKMANAGER_SRC_CONTROLLER_INCLUDE_CONCRETECOMMANDS_H_
-#define TASKMANAGER_SRC_CONTROLLER_INCLUDE_CONCRETECOMMANDS_H_
+#ifndef TASKMANAGER_SRC_CONTROLLER_INCLUDE_CommandWithContextS_H_
+#define TASKMANAGER_SRC_CONTROLLER_INCLUDE_CommandWithContextS_H_
 
 #include "Command.h"
+#include "ContextDTO.h"
 
-class AddCommand : public Command
+class CommandWithContext : public Command
 {
-    using Command::Command;
 public:
-    Model::Response Execute(const std::shared_ptr<View>& view) override;
+    explicit CommandWithContext(const ContextDTO& context);
+public:
+    ContextDTO GetContext();
+public:
+    virtual Model::Response Execute(const std::shared_ptr<Model>& model) override;
+
+private:
+    ContextDTO context_;
 };
 
-class EditCommand : public Command
+class AddCommand : public CommandWithContext
 {
-    using Command::Command;
+    using CommandWithContext::CommandWithContext;
 public:
-    Model::Response Execute(const std::shared_ptr<View>& view) override;
+    Model::Response Execute(const std::shared_ptr<Model>& model) override;
 };
 
-class CompleteCommand : public Command
+class EditCommand : public CommandWithContext
 {
-    using Command::Command;
+    using CommandWithContext::CommandWithContext;
 public:
-    Model::Response Execute(const std::shared_ptr<View>& view) override;
+    Model::Response Execute(const std::shared_ptr<Model>& model) override;
+};
+
+class CompleteCommand : public CommandWithContext
+{
+    using CommandWithContext::CommandWithContext;
+public:
+    Model::Response Execute(const std::shared_ptr<Model>& model) override;
 
 };
 
-class DeleteCommand : public Command
+class DeleteCommand : public CommandWithContext
 {
-    using Command::Command;
+    using CommandWithContext::CommandWithContext;
 public:
-    Model::Response Execute(const std::shared_ptr<View>& view) override;
+    Model::Response Execute(const std::shared_ptr<Model>& model) override;
 
 };
 
-class ShowCommand : public Command
+class ShowCommand : public CommandWithContext
 {
     // TODO Add constructor with filter params
-    using Command::Command;
+    using CommandWithContext::CommandWithContext;
 public:
-    Model::Response Execute(const std::shared_ptr<View>& view) override;
+    Model::Response Execute(const std::shared_ptr<Model>& model) override;
 
 };
-#endif //TASKMANAGER_SRC_CONTROLLER_INCLUDE_CONCRETECOMMANDS_H_
+#endif //TASKMANAGER_SRC_CONTROLLER_INCLUDE_CommandWithContextS_H_
