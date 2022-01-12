79d6fc475c48bac26d8c6a5f6e0e18bf544dbb18
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-12 16:41:49 +0200

    Changed Controller tests

--- tests/controller/ControllerTest.cpp
+++ tests/controller/ControllerTest.cpp
@@ -6,7 +6,6 @@
 #include "gmock/gmock.h"
 #include "../../src/controller/include/Controller.h"
 #include "mocks/ModelMock.h"
-#include "mocks/ViewMock.h"
 #include "mocks/CommandFactoryMock.h"
 #include "mocks/CommandMock.h"
 
@@ -14,28 +13,29 @@ class ControllerTest : public ::testing::Test {};
 
 TEST(ControllerTest, shouldCreate)
 {
-    auto model = std::make_shared<ModelMock>();
     auto factory = std::make_shared<CommandFactoryMock>();
 
-    ASSERT_NO_THROW(Controller(model, factory));
+    ASSERT_NO_THROW(Controller{std::shared_ptr<Model>{new ModelMock}});
 }
 
 TEST(ControllerTest, shouldCreateAndExecuteCommand)
 {
     auto model = std::shared_ptr<Model>{new ModelMock};
-    auto view = std::shared_ptr<View>{new ViewMock};
     auto factory = std::make_shared<CommandFactoryMock>();
-    auto obj = Controller{model, factory};
-    auto command = std::make_shared<CommandMock>(model);
+    auto obj = Controller{model};
+
+    auto command = std::make_shared<CommandMock>();
     auto command_type = CommandType::kNone;
 
-    EXPECT_CALL(*factory, CreateCommand(command_type, model))
+    auto context_dto = std::shared_ptr<ContextDTO>{};
+
+    EXPECT_CALL(*factory, CreateCommand(command_type, context_dto))
         .Times(1)
         .WillOnce(testing::Return(command));
 
-    EXPECT_CALL(*command, Execute(view))
+    EXPECT_CALL(*command, Execute(model))
         .Times(1)
-        .WillOnce(testing::Return(Response{}));
+        .WillOnce(testing::Return(Model::Response::CreateSuccess()));
 
-    obj.Action(view, command_type);
+    obj.Action(command);
 }
