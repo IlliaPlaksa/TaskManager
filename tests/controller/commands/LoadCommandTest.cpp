fa429478e685ad39fa7d167e501fadff11e0a714
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-13 13:30:38 +0200

    Added Save, Load commands' tests

---
+++ tests/controller/commands/LoadCommandTest.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Illia Plaksa on 13.01.2022.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "../../../src/controller/include/ConcreteCommands.h"
+
+#include "../../../src/util/TaskDTOComparers.h"
+
+#include "../mocks/ModelMock.h"
+#include "../mocks/ContextDTOMock.h"
+
+class LoadCommandTest : ::testing::Test {};
+
+// TODO add persister testing
+TEST(LoadCommandTest, shouldExecuteLoad)
+{
+    auto model = std::make_shared<ModelMock>();
+    auto context_dto = std::make_shared<ContextDTOMock>();
+
+    auto variable_set = VariableSet{};
+    variable_set.file_name = "file.txt";
+
+    auto command = LoadCommand{context_dto};
+
+    auto tasks = std::vector<TaskDTO>{};
+
+    EXPECT_CALL(*context_dto, variable_set())
+        .Times(1)
+        .WillOnce(testing::Return(variable_set));
+
+    EXPECT_CALL(*model, Load(tasks))
+        .Times(1)
+        .WillOnce(testing::Return(Model::Response::CreateSuccess()));
+
+    command.Execute(model);
+}
---
+++ tests/controller/commands/SaveCommand.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Illia Plaksa on 13.01.2022.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "../../../src/controller/include/ConcreteCommands.h"
+
+#include "../../../src/util/TaskDTOComparers.h"
+
+#include "../mocks/ModelMock.h"
+#include "../mocks/ContextDTOMock.h"
+
+class SaveCommandTest : ::testing::Test {};
+
+// TODO add persister testing
+TEST(SaveCommandTest, shouldExecuteSave)
+{
+    auto model = std::make_shared<ModelMock>();
+    auto context_dto = std::make_shared<ContextDTOMock>();
+
+    auto variable_set = VariableSet{};
+    variable_set.file_name = "file.txt";
+
+    auto command = SaveCommand{context_dto};
+
+    auto tasks = std::vector<TaskDTO>{};
+
+    EXPECT_CALL(*context_dto, variable_set())
+        .Times(1)
+        .WillOnce(testing::Return(variable_set));
+
+    EXPECT_CALL(*model, Show())
+        .Times(1)
+        .WillOnce(testing::Return(tasks));
+
+    command.Execute(model);
+}
