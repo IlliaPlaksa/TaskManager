7728fa5367176bfd48d64e17f65d3068150d3375
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-13 15:56:44 +0200

    Added new tests for Controller

--- tests/controller/ControllerTest.cpp
+++ tests/controller/ControllerTest.cpp
@@ -27,12 +27,90 @@ TEST(ControllerTest, shouldCreateAndExecuteCommand)
     auto command = std::make_shared<CommandMock>();
 
     EXPECT_CALL(*command, IsReady())
-    .Times(1)
-    .WillOnce(testing::Return(true));
+        .Times(1)
+        .WillOnce(testing::Return(true));
 
     EXPECT_CALL(*command, Execute(model))
         .Times(1)
         .WillOnce(testing::Return(Model::Response::CreateSuccess()));
 
     obj.Action(command);
+}
+
+TEST(ControllerTest, shouldHandleCommandReturnedError)
+{
+    auto model = std::shared_ptr<Model>{new ModelMock};
+    auto factory = std::make_shared<CommandFactoryMock>();
+    auto obj = Controller{model};
+
+    auto command = std::make_shared<CommandMock>();
+
+    EXPECT_CALL(*command, IsReady())
+        .WillRepeatedly(testing::Return(true));
+
+    EXPECT_CALL(*command, Execute(model))
+        .Times(1)
+        .WillOnce(testing::Return(Model::Response::CreateError(Model::Response::ErrorType::FAIL)));
+
+    auto response = obj.Action(command);
+
+    EXPECT_TRUE(response.IsError());
+}
+
+TEST(ControllerTest, shouldHandleCommandWithWrongContext)
+{
+    auto model = std::shared_ptr<Model>{new ModelMock};
+    auto factory = std::make_shared<CommandFactoryMock>();
+    auto obj = Controller{model};
+
+    auto command = std::make_shared<CommandMock>();
+
+    EXPECT_CALL(*command, IsReady())
+        .WillRepeatedly(testing::Return(false));
+
+    auto response = obj.Action(command);
+
+    EXPECT_TRUE(response.IsError());
+}
+
+TEST(ControllerTest, shouldHandleNullptrCommand)
+{
+    auto model = std::shared_ptr<Model>{new ModelMock};
+    auto factory = std::make_shared<CommandFactoryMock>();
+    auto obj = Controller{model};
+
+    auto command = std::shared_ptr<CommandMock>(nullptr);
+
+    auto response = obj.Action(command);
+
+    EXPECT_TRUE(response.IsError());
+}
+
+TEST(ControllerTest, shouldCreateErrorMessage)
+{
+    auto model = std::shared_ptr<Model>{new ModelMock};
+    auto factory = std::make_shared<CommandFactoryMock>();
+    auto obj = Controller{model};
+
+    using ErrorType = Model::Response::ErrorType;
+    auto test = std::vector<std::pair<ErrorType, std::string>>
+    {
+        std::make_pair(ErrorType::INVALID_ID, "Invalid ID passed"),
+        std::make_pair(ErrorType::EMPTY_TITLE, "Empty title of Task passed"),
+        std::make_pair(ErrorType::NON_EXISTING_PARENT_ID, "Non-existing parent ID passed"),
+        std::make_pair(ErrorType::FAIL, "Something went wrong"),
+    };
+
+    auto command = std::make_shared<CommandMock>();
+
+    EXPECT_CALL(*command, IsReady())
+    .WillRepeatedly(testing::Return(true));
+
+    for (const auto& elem : test)
+    {
+        EXPECT_CALL(*command, Execute(model))
+        .WillOnce(testing::Return(Model::Response::CreateError(elem.first)));
+        auto response = obj.Action(command);
+        EXPECT_EQ(response.error(), elem.second);
+    }
 }
