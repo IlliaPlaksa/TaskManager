6c8af19c252a0f4bf4d63cbb64c8b4eb82716c69
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-14 15:08:49 +0200

    Changed tests for FilePersister

--- tests/persistence/TaskPersistenceTest.cpp
+++ tests/persistence/TaskPersistenceTest.cpp
@@ -3,17 +3,17 @@
 //
 
 #include "gtest/gtest.h"
-#include "../../src/persistence/include/TaskPersister.h"
+#include "../../src/persistence/include/FilePersister.h"
 #include "TaskDTO.pb.h"
 #include "../../src/util/TaskDTOComparers.h"
 
-class TaskPersisterTest : public ::testing::Test {};
+class FilePersisterTest : public ::testing::Test {};
 
 TEST(TaskPersisterTest, shouldSerializeAndDeserialize)
 {
     auto tasks = std::vector<TaskDTO>{};
 
-    for (size_t i = 0; i < 5; i++)
+    for (size_t i = 0 ; i < 5 ; i++)
     {
         auto tmp = TaskDTO{};
         auto task = tmp.mutable_task();
@@ -26,13 +26,13 @@ TEST(TaskPersisterTest, shouldSerializeAndDeserialize)
 
     std::string file_name = "test_file.txt";
 
-    auto persister = TaskPersister{};
-    persister.SerializeTasksToFile(file_name, tasks);
-    auto deserialized = persister.DeserializeTasksFromFile(file_name);
+    auto persister = FilePersister{file_name};
+    persister.Save(tasks);
+    auto deserialized = persister.Load();
 
     ASSERT_TRUE(deserialized);
     ASSERT_EQ(tasks.size(), deserialized->size());
 
-    for (const auto &elem: deserialized.value())
+    for (const auto& elem : deserialized.value())
         EXPECT_TRUE(tasks.end() != std::find(tasks.cbegin(), tasks.cend(), elem));
 }
