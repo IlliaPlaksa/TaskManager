04a1d8b2a822b212429c7e8c384362f2ffa5bd68
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-17 12:59:04 +0200

    Added PersisterMock

---
+++ tests/mocks/PersisterMock.h
@@ -0,0 +1,18 @@
+//
+// Created by Illia Plaksa on 17.01.2022.
+//
+
+#ifndef TASKMANAGER_TESTS_MOCKS_PERSISTERMOCK_H_
+#define TASKMANAGER_TESTS_MOCKS_PERSISTERMOCK_H_
+
+#include "gmock/gmock.h"
+#include "common/include/Persister.h"
+
+class TaskPersisterMock : public TaskPersister
+{
+public:
+    MOCK_METHOD(std::optional<std::vector<TaskDTO>>, Load, (), (override));
+    MOCK_METHOD(bool, Save, (const std::vector<TaskDTO>& tasks), (override));
+};
+
+#endif //TASKMANAGER_TESTS_MOCKS_PERSISTERMOCK_H_
