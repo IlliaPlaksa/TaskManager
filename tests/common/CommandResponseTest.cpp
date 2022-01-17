8f9bf969035ec2053b220cad4063dbccfa68006f
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-17 11:50:43 +0200

    Added CommandResponseTest

--- tests/common/CommandResponseTest.cpp
+++ tests/common/CommandResponseTest.cpp
@@ -4,3 +4,31 @@
 
 #include "common/include/CommandResponse.h"
 
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+
+class CommandResponseTest : testing::Test
+{
+};
+
+TEST(CommandResponseTest, shouldWorkIsErrorMethod)
+{
+    auto response = CommandResponse{};
+    response.model_response = ModelResponse::Error(ModelResponse::ErrorType::FAIL);
+
+    EXPECT_TRUE(response.IsError());
+
+    response = CommandResponse{};
+
+    EXPECT_TRUE(response.IsError());
+
+    response = CommandResponse{};
+    response.tasks = std::vector<TaskDTO>{};
+
+    EXPECT_FALSE(response.IsError());
+
+    response = CommandResponse{};
+    response.model_response = ModelResponse::Success();
+    EXPECT_FALSE(response.IsError());
+}
