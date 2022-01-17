ff6f427cd0ee72dc0e2e8777e54d4242159be8d4
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-17 16:50:26 +0200

    Added ContextTest

---
+++ tests/cli/ContextTest.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Illia Plaksa on 17.01.2022.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "cli/include/Context.h"
+
+class ContextTest : ::testing::Test
+{
+};
+
+TEST(ContextTest, shouldCreate)
+{
+    auto obj = Context{};
+
+    EXPECT_TRUE(obj.GetVariableSet());
+    EXPECT_TRUE(obj.GetStorage());
+    EXPECT_FALSE(obj.GetError());
+}
+
+TEST(ContextTest, shouldSetError)
+{
+    auto obj = Context{};
+    auto message = "Simple error message";
+    obj.SetError(message);
+
+    EXPECT_EQ(obj.GetError(), message);
+}
