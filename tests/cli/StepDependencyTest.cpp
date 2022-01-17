19133e75af849720dcd2ac3b30a180992d047930
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-17 16:30:17 +0200

    Added StepDependencyTest

---
+++ tests/cli/StepDependencyTest.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Illia Plaksa on 17.01.2022.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "cli/include/StepDependency.h"
+
+#include "mocks/ConsoleManipulatorMock.h"
+#include "mocks/StepFactoryMock.h"
+
+class StepDependencyTest : ::testing::Test
+{
+};
+
+TEST(StepDependencyTest, shouldCreate)
+{
+    auto console = std::make_shared<ConsoleManipulatorMock>();
+    auto factory = std::make_shared<StepFactoryMock>(console);
+    auto obj = StepDependency{factory, console};
+
+    EXPECT_EQ(obj.step_factory(), factory);
+    EXPECT_EQ(obj.console_manipulator(), console);
+}
+
+
