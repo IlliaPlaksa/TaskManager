8ef9fcca1f51eddd20414f1e75ed0b806e93b2bb
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-18 17:08:52 +0200

    Added ConsoleManipulatorTest

---
+++ tests/cli/ConsoleManipulatorTest.cpp
@@ -0,0 +1,107 @@
+//
+// Created by Illia Plaksa on 18.01.2022.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "cli/include/ConsoleManipulator.h"
+
+class ConsoleManipulatorTest : ::testing::Test
+{
+};
+
+TEST(ConsoleManipulatorTest, shouldWriteLine)
+{
+    auto ostream = std::stringstream{};
+    auto istream = std::stringstream{};
+
+    auto console = ConsoleManipulator{ostream, istream};
+
+    auto message = "Console message";
+
+    console.WriteLine(message);
+
+    auto output = std::string{};
+
+    getline(ostream, output);
+
+    EXPECT_EQ(output, message);
+}
+
+TEST(ConsoleManipulatorTest, shouldWriteError)
+{
+    auto ostream = std::stringstream{};
+    auto istream = std::stringstream{};
+    auto console = ConsoleManipulator{ostream, istream};
+
+    auto message = std::string{"Error message"};
+
+    console.WriteError(message);
+
+    auto input = std::string{};
+
+    getline(ostream, input);
+
+    EXPECT_EQ(input, "[Error]\t" + message);
+}
+
+TEST(ConsoleManipulatorTest, shouldReadline)
+{
+    auto ostream = std::stringstream{};
+    auto istream = std::stringstream{};
+
+    auto console = ConsoleManipulator{ostream, istream};
+    auto input = std::string{"Input string"};
+    auto output = std::string();
+
+    istream << input;
+
+    auto actual_input = console.ReadLine();
+
+    getline(ostream, output);
+
+    EXPECT_EQ(output, std::string(" > "));
+    EXPECT_EQ(actual_input, input);
+
+    ostream = std::stringstream{};
+    istream = std::stringstream{};
+
+    istream << input;
+
+    auto message = std::string("Message");
+    actual_input = console.ReadLine(message);
+
+    getline(ostream, output);
+
+    EXPECT_EQ(output, std::string(' ' + message + "> "));
+    EXPECT_EQ(actual_input, input);
+}
+
+TEST(ConsoleManipulatorTest, shouldResetPromps)
+{
+    auto ostream = std::stringstream{};
+    auto istream = std::stringstream{};
+
+    auto console = ConsoleManipulator{ostream, istream};
+
+    auto message = std::string{"Prompt"};
+
+    console.ResetPrompt(message);
+
+    console.ReadLine();
+
+    auto output  = std::string{};
+    getline(ostream, output);
+
+    EXPECT_EQ(output, message + " > ");
+
+    ostream = std::stringstream{};
+    istream = std::stringstream{};
+
+    console.ResetPrompt();
+    console.ReadLine();
+    getline(ostream, output);
+
+    EXPECT_EQ(output, " > ");
+}
+
