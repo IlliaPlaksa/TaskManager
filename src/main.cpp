5e158754dc7fc2621875b1c85471c8f0fccfce16
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-18 17:08:18 +0200

    Changed main

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main()
 
     // auto controller = std::shared_ptr<Model>(new ModelController{model});
 
-    auto console_manipulator = std::make_shared<ConsoleManipulator>(std::cout);
+    auto console_manipulator = std::make_shared<ConsoleManipulator>(std::cout, std::cin);
     auto step_factory = std::shared_ptr<StepFactory>{new StepFactory{console_manipulator}};
 
     auto view = std::shared_ptr<View>{new StepMachine{step_factory, model}};
