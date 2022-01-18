ff9e9255f403b164e977ac557dc5f442512d6514
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-18 12:16:59 +0200

    Added StepMock to tests

---
+++ tests/mocks/StepMock.h
@@ -0,0 +1,17 @@
+//
+// Created by Illia Plaksa on 18.01.2022.
+//
+
+#ifndef TASKMANAGER_TESTS_MOCKS_STEPMOCK_H_
+#define TASKMANAGER_TESTS_MOCKS_STEPMOCK_H_
+
+#include "gmock/gmock.h"
+#include "cli/include/Step.h"
+
+class StepMock : public Step
+{
+public:
+    MOCK_METHOD(StepResult, Execute, (Context& context), (override));
+};
+
+#endif //TASKMANAGER_TESTS_MOCKS_STEPMOCK_H_
