e88f9da7a2ea3ac7f1fdff0cd409b8659aca5cb1
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-19 15:33:04 +0200

    Updated Tests due to labels feature

--- tests/model/TaskNodeTest.cpp
+++ tests/model/TaskNodeTest.cpp
@@ -17,17 +17,23 @@ TEST(TaskNodeTest, shouldCreate)
     auto title = "Simple task name";
     auto due_date = time(nullptr);
     auto priority = Task::Priority::Task_Priority_kMedium;
-    auto label = "Simple task label";
+    auto labels = std::vector<std::string>{"Label", "SecondLabel"};
     auto parent_id = *CreateTaskId(1234);
 
-    auto simple_task = *CreateTask(title, due_date, priority, label);
+    auto simple_task = *CreateTask(title, due_date, priority, labels);
     auto task = TaskNode::Create(simple_task, parent_id);
 
     EXPECT_EQ(task.GetTask().title(), title);
     EXPECT_EQ(task.GetTask().due_date().seconds(), due_date);
     EXPECT_EQ(task.GetTask().priority(), priority);
-    EXPECT_EQ(task.GetTask().label(), label);
     EXPECT_EQ(*task.GetParentId(), parent_id);
+
+    auto actual_labels = task.GetTask().labels();
+
+    for(auto iter = actual_labels.begin(); iter != actual_labels.end(); ++iter)
+    {
+        EXPECT_TRUE(std::find(labels.begin(), labels.end(), *iter) != labels.end());
+    }
 }
 
 
