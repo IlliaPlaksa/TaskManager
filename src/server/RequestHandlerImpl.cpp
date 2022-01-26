c5516b351a3d46741eb0636ebf349448962bb2f9
IlliaPLaksa <illia.plaksa@teamdev.com>
2022-01-26 14:27:14 +0200

    Added RequestHandlerImpl class

---
+++ src/server/RequestHandlerImpl.cpp
@@ -0,0 +1,142 @@
+//
+// Created by Illia Plaksa on 26.01.2022.
+//
+
+#include "RequestHandlerImpl.h"
+
+RequestHandlerImpl::RequestHandlerImpl(std::unique_ptr<Model> model)
+    :
+    model_(std::move(model))
+{
+
+}
+
+grpc::Status RequestHandlerImpl::Add(::grpc::ServerContext* context,
+                                     const ::Task* request,
+                                     ::service::Response* response)
+{
+    const auto& task = *request;
+    auto result = model_->Add(task);
+
+    *response = ModelResponseToServiceResponse(result);
+    return ::grpc::Status::OK;
+}
+grpc::Status RequestHandlerImpl::AddSubTask(::grpc::ServerContext* context,
+                                            const ::TaskDTO* request,
+                                            ::service::Response* response)
+{
+    const auto& task = request->task();
+    const auto& parent_id = request->parent_id();
+
+    auto result = model_->AddSubTask(task, parent_id);
+    *response = ModelResponseToServiceResponse(result);
+    return ::grpc::Status::OK;
+}
+grpc::Status RequestHandlerImpl::Edit(::grpc::ServerContext* context,
+                                      const ::TaskDTO* request,
+                                      ::service::Response* response)
+{
+    const auto& id = request->id();
+    const auto& task = request->task();
+
+    auto result = model_->Edit(id, task);
+
+    *response = ModelResponseToServiceResponse(result);
+    return ::grpc::Status::OK;
+}
+grpc::Status RequestHandlerImpl::EditSubTask(::grpc::ServerContext* context,
+                                             const ::TaskDTO* request,
+                                             ::service::Response* response)
+{
+    const auto& id = request->id();
+    const auto& task = request->task();
+    const auto& parent_id = request->id();
+
+    auto result = model_->EditSubTask(id, task, parent_id);
+
+    *response = ModelResponseToServiceResponse(result);
+    return ::grpc::Status::OK;
+}
+grpc::Status RequestHandlerImpl::Complete(::grpc::ServerContext* context,
+                                          const ::TaskId* request,
+                                          ::service::Response* response)
+{
+    const auto& id = *request;
+
+    auto result = model_->Complete(id);
+
+    *response = ModelResponseToServiceResponse(result);
+    return ::grpc::Status::OK;
+}
+grpc::Status RequestHandlerImpl::Delete(::grpc::ServerContext* context,
+                                        const ::TaskId* request,
+                                        ::service::Response* response)
+{
+    const auto& id = *request;
+
+    auto result = model_->Delete(id);
+
+    *response = ModelResponseToServiceResponse(result);
+    return ::grpc::Status::OK;
+}
+grpc::Status RequestHandlerImpl::Show(::grpc::ServerContext* context,
+                                      const ::service::BlankMessage* request,
+                                      ::grpc::ServerWriter<::TaskDTO>* writer)
+{
+    auto tasks = model_->Show();
+
+    for (const auto& task: tasks)
+    {
+        writer->Write(task);
+    }
+
+    return ::grpc::Status::OK;
+}
+grpc::Status RequestHandlerImpl::ShowParents(::grpc::ServerContext* context,
+                                             const ::service::BlankMessage* request,
+                                             ::grpc::ServerWriter<::TaskDTO>* writer)
+{
+    auto tasks = model_->ShowParents();
+
+    for (const auto& task: tasks)
+    {
+        writer->Write(task);
+    }
+
+    return ::grpc::Status::OK;
+}
+grpc::Status RequestHandlerImpl::ShowChild(::grpc::ServerContext* context,
+                                           const ::TaskId* request,
+                                           ::grpc::ServerWriter<::TaskDTO>* writer)
+{
+    auto parent_id = *request;
+    auto tasks = model_->ShowChild(parent_id);
+
+    for (const auto& task: tasks)
+    {
+        writer->Write(task);
+    }
+
+    return ::grpc::Status::OK;
+}
+
+grpc::Status RequestHandlerImpl::Load(::grpc::ServerContext* context,
+                                      ::grpc::ServerReader<::TaskDTO>* reader,
+                                      ::service::Response* response)
+{
+    auto tasks = std::vector<TaskDTO>{};
+
+    auto tmp_task = TaskDTO();
+
+    while (reader->Read(&tmp_task))
+    {
+        tasks.emplace_back(tmp_task);
+    }
+
+    auto result = model_->Load(tasks);
+
+    *response = ModelResponseToServiceResponse(result);
+
+    return ::grpc::Status::OK;
+}
+
---
+++ src/server/RequestHandlerImpl.h
@@ -0,0 +1,49 @@
+//
+// Created by Illia Plaksa on 26.01.2022.
+//
+
+#ifndef TASKMANAGER_SRC_SERVER_REQUESTHANDLERIMPL_H_
+#define TASKMANAGER_SRC_SERVER_REQUESTHANDLERIMPL_H_
+
+#include "Service.grpc.pb.h"
+#include "common/include/Model.h"
+#include "util/Response/ResponseConverters.h"
+
+class RequestHandlerImpl : public service::RequestHandler::Service
+{
+public:
+    explicit RequestHandlerImpl(std::unique_ptr<Model> model);
+
+public:
+    grpc::Status Add(::grpc::ServerContext* context, const ::Task* request, ::service::Response* response) override;
+
+    grpc::Status AddSubTask(::grpc::ServerContext* context,
+                            const ::TaskDTO* request,
+                            ::service::Response* response) override;
+    grpc::Status Edit(::grpc::ServerContext* context, const ::TaskDTO* request, ::service::Response* response) override;
+    grpc::Status EditSubTask(::grpc::ServerContext* context,
+                             const ::TaskDTO* request,
+                             ::service::Response* response) override;
+    grpc::Status Complete(::grpc::ServerContext* context,
+                          const ::TaskId* request,
+                          ::service::Response* response) override;
+    grpc::Status Delete(::grpc::ServerContext* context,
+                        const ::TaskId* request,
+                        ::service::Response* response) override;
+    grpc::Status Show(::grpc::ServerContext* context,
+                      const ::service::BlankMessage* request,
+                      ::grpc::ServerWriter<::TaskDTO>* writer) override;
+    grpc::Status ShowParents(::grpc::ServerContext* context,
+                             const ::service::BlankMessage* request,
+                             ::grpc::ServerWriter<::TaskDTO>* writer) override;
+    grpc::Status ShowChild(::grpc::ServerContext* context,
+                           const ::TaskId* request,
+                           ::grpc::ServerWriter<::TaskDTO>* writer) override;
+    grpc::Status Load(::grpc::ServerContext* context,
+                      ::grpc::ServerReader<::TaskDTO>* reader,
+                      ::service::Response* response) override;
+private:
+    std::unique_ptr<Model> model_;
+};
+
+#endif //TASKMANAGER_SRC_SERVER_REQUESTHANDLERIMPL_H_
