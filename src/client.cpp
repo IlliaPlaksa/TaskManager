7087e9abdba78a4eb145f8a071f569fdf5fd4f72
IlliaPlaksa <93770275+IlliaPlaksa@users.noreply.github.com>
2022-02-03 17:45:37 +0200

    Added program options to client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -7,12 +7,82 @@
 
 #include "Logging.h"
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+std::string CreateAddress(const std::string& host, const std::string& port)
+{
+    return host + ":" + port;
+}
+
+boost::log::trivial::severity_level CreateSeverityLevelFromString(const std::string& level)
+{
+    auto result = boost::log::trivial::severity_level{};
+
+    if (!boost::log::trivial::from_string(level.c_str(), level.size(), result))
+        result = boost::log::trivial::severity_level::info;
+
+    return result;
+}
+
 int main(int argc, char** argv)
 {
     logging::init("client.log",boost::log::trivial::severity_level::info);
 
-    std::string client_address{"localhost:50051"};
-    auto channel = grpc::CreateChannel(client_address, grpc::InsecureChannelCredentials());
+    std::string host; // default address of server
+    std::string port;
+
+    auto log_file_name = std::string{"client.log"};
+    auto severity_level = boost::log::trivial::severity_level{};
+    std::string verbosity_str;
+try
+    {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+
+            ("verbosity",
+             po::value<std::string>(&verbosity_str)->default_value("info"),
+           "sets log verbosity: debug, info, warn, error, fatal")
+
+            ("host",
+             po::value<std::string>(&host)->default_value("localhost"),
+             "sets server address")
+
+            ("port",
+             po::value<std::string>(&port)->default_value("50051"),
+             "sets port for sending messages");
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        if (vm.count("help"))
+        {
+            std::cout << desc << "\n";
+            return 1;
+        }
+
+        severity_level = logging::CreateSeverityLevelFrom(verbosity_str);
+
+        std::cout << "Log verbosity set to " << boost::log::trivial::to_string(severity_level) << ".\n";
+        std::cout << "Server address was set to " << host << ".\n";
+        std::cout << "Port was set to " << port << ".\n";
+
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+
+    logging::init(log_file_name, severity_level);
+
+    auto channel_address = CreateAddress(host, port);
+
+    auto channel = grpc::CreateChannel(
+        channel_address, grpc::InsecureChannelCredentials()
+    );
     auto stub = service::RequestHandler::NewStub(channel);
 
     auto controller = std::make_shared<RemoteController>(std::move(stub));
@@ -22,7 +92,7 @@ int main(int argc, char** argv)
 
     auto view = std::shared_ptr<View>{new StepMachine{step_factory, controller}};
 
-    BOOST_LOG_TRIVIAL(info) << "Client started at " << client_address;
+    BOOST_LOG_TRIVIAL(info) << "Client started with channel at " << channel_address;
 
     view->Run();
 
