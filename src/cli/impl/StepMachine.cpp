c55751b47501062fbccf2553a974919175f3094a
IlliaPlaksa <93770275+IlliaPlaksa@users.noreply.github.com>
2022-02-07 15:46:07 +0200

    Program options (#6)
    * Added Logging file to project
    
    * Added usage of Logging file in server.cpp
    
    * Added include of Logging.h to TaskManager.h
    
    * Added logging of Show and Load methods in TaskManager
    
    * Changed logging::init function signature
    
    * Changed logging::init call in server.cpp
    
    * Changed client.cpp logging
    
    * Added include of logging to ConcreteCommands
    
    * Added logging to ShowCommand Execute
    
    * Added logging to SetContextFromCommandResponse in StepMachine
    
    * Fixed SetContextFromCommandResponse's logs
    
    * Added logging to LoadCommand's Execute
    
    * Changed message log level in TaskManager Show method
    
    * Changed log levels in StepMachine
    
    * Changed log levels in Show, Load commands
    
    * Added main.cpp to tests with enabled logging
    
    * Added program options to client.cpp
    
    * Moved CreateSeverityLevelFrom function to Logging.h
    
    * Added program options to server.cpp
    
    * Rebased from main
    
    Added usage of Logging file in server.cpp
    
    Added include of Logging.h to TaskManager.h
    
    Added logging of Show and Load methods in TaskManager
    
    Changed logging::init function signature
    
    Changed logging::init call in server.cpp
    
    Changed client.cpp logging
    
    Added include of logging to ConcreteCommands
    
    Added logging to ShowCommand Execute
    
    Added logging to SetContextFromCommandResponse in StepMachine
    
    Fixed SetContextFromCommandResponse's logs
    
    Added logging to LoadCommand's Execute
    
    Changed message log level in TaskManager Show method
    
    Changed log levels in StepMachine
    
    Changed log levels in Show, Load commands
    
    Added main.cpp to tests with enabled logging
    
    * Added program options to client.cpp
    
    * Moved CreateSeverityLevelFrom function to Logging.h
    
    * Added program options to server.cpp
    
    Co-authored-by: IlliaPLaksa <56171294+Iluhap@users.noreply.github.com>

--- src/cli/impl/StepMachine.cpp
+++ src/cli/impl/StepMachine.cpp
@@ -62,15 +62,21 @@ void StepMachine::SetContextFromCommandResponse(const CommandResponse& response)
 
         auto error_message = CreateErrorMessage(*response.model_response->error());
         context_.SetError(error_message);
+
+        BOOST_LOG_TRIVIAL(debug) << "Command returned error message: " << error_message << ".";
     }
     else
     {
+        BOOST_LOG_TRIVIAL(debug) << "Command returned tasks";
+
         auto tasks = response.tasks;
         auto storage = context_.GetStorage();
         if (tasks.has_value() and storage)
         {
             *storage = tasks.value();
-            BOOST_LOG_TRIVIAL(debug) << "Loaded " << tasks->GetTasks().size() << " tasks to Context";
+            BOOST_LOG_TRIVIAL(debug) << "Loaded " << tasks->GetTasks().size() << "tasks to Context";
         }
+        else
+            BOOST_LOG_TRIVIAL(debug) << "Command was successfully executed.";
     }
 }
