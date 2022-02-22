15166517190ac1d5381c82bd0aa7f5f145e1de02
IlliaPlaksa <93770275+IlliaPlaksa@users.noreply.github.com>
2022-02-22 17:55:12 +0200

    Added documentation to ModelResponse

--- src/common/include/ModelResponse.h
+++ src/common/include/ModelResponse.h
@@ -7,9 +7,18 @@
 
 #include <optional>
 
+/**
+ * @brief \b Represents response to a request to the \b Model
+ * @details Provides a representation of all responses to requests to the \b Model
+ * @author Plaksa Illia
+ */
 class ModelResponse
 {
 public:
+
+    /**
+    * @brief \b Represents type of \b Errors that can be caused by request to \b Model
+    */
     enum class ErrorType
     {
         INVALID_ID,
@@ -20,6 +29,9 @@ public:
         // TODO add more error types
     };
 
+    /**
+    * @brief \b Represents status of \b Response of \b Model
+    */
     enum class Status
     {
         kSuccess,
